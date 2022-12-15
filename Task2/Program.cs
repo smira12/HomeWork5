7a473134c9d97db7d12554e2e232c9263de03203
smira12 <m1ronsa89@gmail.com>
2022-12-15 10:14:36 +0300

    Add Task №36

---
+++ Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число.");
+        }
+    }
+    return result;
+}
+
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int ParityNumberArray(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int dimension = GetNumber("Введите размерность массива: ");
+int[] array = InitArray(dimension);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {ParityNumberArray(array)}");
